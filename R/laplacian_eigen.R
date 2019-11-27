d0eb7671d8cb13b51832d1fcd8c6c7fbd2480235
Jianing <jianing.stat.tamu.edu>
2019-11-27 11:33:55 -0600

    fixed bugs related to laplacian

--- R/laplacian_eigen.R
+++ R/laplacian_eigen.R
@@ -1,4 +1,4 @@
-laplacian_eigen <- function(X, no_dims = 2, k = 12, sigma = 1){
+laplacian_eigen <- function(X, no_dims = 2, k = 10, sigma = 1){
   # LAPLACIAN_EIGEN Performs non-linear dimensionality reduction using Laplacian Eigenmaps
   #
   # [mappedX, mapping] = laplacian_eigen(X, no_dims, k, sigma, eig_impl)
@@ -10,17 +10,22 @@ laplacian_eigen <- function(X, no_dims = 2, k = 12, sigma = 1){
   # neighbours in the graph (default = 12).
   # The reduced data is returned in the matrix mappedX.
   #
-  source('L2_distance.R')
-  source('components.R')
+  source('R/L2_distance.R')
+  source('R/my_components.R')
+  source('R/knnsearch_slow.R')
+  source('R/createKnnGraph.R')
   library('FNN')
   library('geigen')
   
   # construct neighborhood graph
-  G = get.knn(X, k)
-  G = G^2
+  #G = get.knn(X, k)
+  #G = G^2
+  #G = G/pmax(G)
+  G = knnsearch_slow(X, k)$matrix
+  G = as.matrix(G)
   G = G/max(G)
   # Only embed largest connected component of the neighborhood graph
-  blocks = components(G)
+  blocks = my_components(G)
   count = matrix(0, 1, max(blocks))
   for (i in 1:max(blocks)){
     count[i] = length(which(blocks == i))
@@ -37,9 +42,17 @@ laplacian_eigen <- function(X, no_dims = 2, k = 12, sigma = 1){
   D = diag(rowSums(G));
   
   # Compute Laplacian
-  L = D - G;
-  L(is.nan(L)) = 0; D(is.nan(D)) = 0;
-  L(is.infinite(L)) = 0; D(is.infinite(D)) = 0;
+  L = D - G
+  L = matrix(L, ncol = dim(L)[1])
+  D = matrix(D, ncol = dim(D)[1])
+  L[is.na(L)] = 0
+  G[is.na(G)] = 0
+  L[is.infinite(L)] = 0
+  G[is.infinite(G)] = 0
+  #L[which.nan(L)] = 0
+  #D(is.nan(D)) = 0
+  #L(is.infinite(L)) = 0
+  #D(is.infinite(D)) = 0
   
   # Construct eigenmaps (solve Ly = lambda*Dy)
   output = geigen(L, D, only.values = FALSE)
@@ -47,10 +60,10 @@ laplacian_eigen <- function(X, no_dims = 2, k = 12, sigma = 1){
   # only need bottom (no_dims + 1) eigenvectors
   lambda = output$values
   vectors = output$vectors
-  magnitude = colSums(vectors)
-  vec_index = sort(magnitude, decreasing = TRUE, index.return = TRUE)$ix
-  lambda = lambda[vec_index[1 : (no_dims + 1)]]
-  vectors = vectors[, vec_index[1 : (no_dims + 1)]]
+  # magnitude = colSums(vectors)
+  # vec_index = sort(magnitude, decreasing = TRUE, index.return = TRUE)$ix
+  # lambda = lambda[vec_index[1 : (no_dims + 1)]]
+  # vectors = vectors[, vec_index[1 : (no_dims + 1)]]
 
   # Sort eigenvectors in ascending order
   output2 = sort(lambda, decreasing = FALSE, index.return = TRUE)
